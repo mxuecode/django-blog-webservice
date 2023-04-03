78b1090bc94e231c1b8b379c9801493718c4ac17
mxue0309 <mxue0309@gmail.com>
2023-04-03 14:25:51 +0800

    fix sercurity

--- firstsite/settings.py
+++ firstsite/settings.py
@@ -20,10 +20,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-g)@4e$da8&=9++b3&gc1^k*y%$5_qt@r4tkr@ku4p25@5h2i6r'
-
+# here need to trun to modify it
+SECRET_KEY = '' 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
